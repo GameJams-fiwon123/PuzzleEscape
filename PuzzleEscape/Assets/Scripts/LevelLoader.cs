f0dfdf4ccddf07a1099d73446c70cda5e8bb5378
fiwon123 <felipeinoue@yahoo.com.br>
2019-07-22 11:19:57 -0300

    Credits Created

---
+++ PuzzleEscape/Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void LoadLevel()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void LoadCredits()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+}
