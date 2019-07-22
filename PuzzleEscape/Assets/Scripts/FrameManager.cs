d64181f42aa3d288b431ce4edd099d7925a660b3
fiwon123 <felipeinoue@yahoo.com.br>
2019-07-22 12:31:23 -0300

    Base Level 0

---
+++ PuzzleEscape/Assets/Scripts/FrameManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrameManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject cam;
+
+    private int posXFrame0 = 0;
+    private int posXFrame1 = 18;
+    private int posXFrame2 = 36;
+    private int posXFrame3 = 54;
+
+
+    public void MoveCamToFrame(int frame)
+    {
+        Vector3 newPos = cam.transform.position;
+
+        switch (frame)
+        {
+            case 0:
+                newPos.x = posXFrame0;
+                break;
+            case 1:
+                newPos.x = posXFrame1;
+                break;
+            case 2:
+                newPos.x = posXFrame2;
+                break;
+            case 3:
+                newPos.x = posXFrame3;
+                break;
+        }
+
+        cam.transform.position = newPos;
+    }
+}
---
+++ PuzzleEscape/Assets/Teleport.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour
+{
+    [Range(0, 3)]
+    [SerializeField] int frame = 0;
+
+    [SerializeField] bool teleportPlayer = false;
+    [SerializeField] float posX = 0;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        FindObjectOfType<FrameManager>().MoveCamToFrame(frame);
+
+        if (teleportPlayer)
+        {
+            Player player = FindObjectOfType<Player>();
+
+            Vector3 newPos = player.transform.position ;
+            newPos.x = posX;
+
+            player.transform.position = newPos;
+        }
+    }
+}
