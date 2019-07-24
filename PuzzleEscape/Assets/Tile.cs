0201985c000a514b3157fdea6fb317506be4a687
fiwon123 <felipeinoue@yahoo.com.br>
2019-07-24 15:08:29 -0300

    Quadro Base Finish

---
+++ PuzzleEscape/Assets/Tile.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    [SerializeField] int id = 0;
+
+    private void OnMouseDown()
+    {
+        FindObjectOfType<TilesManager>().ClickTile(id);
+    }
+
+
+}
---
+++ PuzzleEscape/Assets/TilesManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TilesManager : MonoBehaviour
+{
+
+    [SerializeField] int[] sequencia;
+    int index = 0;
+    bool isFinish = false;
+
+    public void ClickTile(int id)
+    {
+        if (!isFinish)
+        {
+            if (sequencia[index] == id)
+            {
+                index++;
+                if (index == sequencia.Length)
+                {
+                    GetComponent<Animator>().Play("OpenQuadro");
+                    isFinish = true;
+                }
+            }
+            else
+            {
+                index = 0;
+                print("Errado");
+            }
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        index = 0;
+        print("Errado");
+    }
+}
