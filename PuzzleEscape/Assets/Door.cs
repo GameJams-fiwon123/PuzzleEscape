1b0e00c847276b0ec2f917a50883479034afc78f
fiwon123 <felipeinoue@yahoo.com.br>
2019-07-24 13:05:39 -0300

    Door completed

---
+++ PuzzleEscape/Assets/Door.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    [SerializeField] Sprite spriteOpenDoor;
+
+    [SerializeField] AudioClip audioDestrancar;
+    bool isLocked = true;
+
+    private void OnMouseDown()
+    {
+        if (isLocked)
+            GetComponent<AudioSource>().Play();
+        else
+        {
+            GetComponent<AudioSource>().clip = audioDestrancar;
+            GetComponent<AudioSource>().Play();
+            GetComponent<SpriteRenderer>().sprite = spriteOpenDoor;
+        }
+    }
+}
