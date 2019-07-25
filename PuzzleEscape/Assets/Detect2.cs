78ec9bb7024d689f0027ea566567bcba38052c6c
fiwon123 <felipeinoue@yahoo.com.br>
2019-07-25 20:32:46 -0300

    Completed puzzle paper

---
+++ PuzzleEscape/Assets/Detect2.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Detect2 : MonoBehaviour
+{
+    private void OnMouseDown()
+    {
+        if (FindObjectOfType<ItemsManager>().GetCurrentItem() != null)
+        {
+            if(FindObjectOfType<ItemsManager>().GetCurrentItem().GetId() == 5)
+            {
+                transform.GetChild(0).gameObject.SetActive(true);
+            }
+        }
+    }
+}
---
+++ PuzzleEscape/Assets/Detect5.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Detect5 : MonoBehaviour
+{
+    private void OnMouseDown()
+    {
+        if (FindObjectOfType<ItemsManager>().GetCurrentItem() != null)
+        {
+            if (FindObjectOfType<ItemsManager>().GetCurrentItem().GetId() == 4)
+            {
+                transform.GetChild(0).gameObject.SetActive(true);
+            }
+        }
+    }
+}
