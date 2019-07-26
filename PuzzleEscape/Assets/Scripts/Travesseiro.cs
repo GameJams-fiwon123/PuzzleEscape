c3a1226fb3b69c97071e6a2f9b0c1eb40d0e4ef0
fiwon123 <felipeinoue@yahoo.com.br>
2019-07-26 19:21:19 -0300

    Fix problem in fram4

--- PuzzleEscape/Assets/Scripts/Travesseiro.cs
+++ PuzzleEscape/Assets/Scripts/Travesseiro.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Travesseiro : MonoBehaviour
 {
-    [SerializeField] GameObject item;
+    [SerializeField] GameObject item = null;
 
     private void OnMouseDown()
     {
